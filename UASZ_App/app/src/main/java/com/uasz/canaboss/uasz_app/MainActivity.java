02e71f6ddcfe07b0fd91cc7a572f658187679588
OBrien123 <b2mnumero23@gmail.com>
2024-06-03 17:29:55 +0000

    nouveau chargement le 03/06/2024

--- UASZ_App/app/src/main/java/com/uasz/canaboss/uasz_app/MainActivity.java
+++ UASZ_App/app/src/main/java/com/uasz/canaboss/uasz_app/MainActivity.java
@@ -1,24 +1,84 @@
 package com.uasz.canaboss.uasz_app;
 
+import static android.content.ContentValues.TAG;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        CardView cardView1 = findViewById(R.id.card_view_1);
+        CardView cardView2 = findViewById(R.id.card_view_2);
+        CardView cardView3 = findViewById(R.id.card_view_3);
+        CardView cardView4 = findViewById(R.id.card_view_4);
+        CardView cardView5 = findViewById(R.id.card_view_5);
+        CardView cardView6 = findViewById(R.id.card_view_6);
+
+        cardView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PremierActivity.class);
+                startActivity(intent);
+            }
         });
+
+        cardView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DeuxiemeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cardView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TroisiemeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cardView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, QuatriemeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cardView5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CinquiemeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cardView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SixiemeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
